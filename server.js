760f9c33049d4ae288ed98e6f5ed45d5942b4d9d
Akshaj Dev <akshajdevkv@gmail.com>
2025-05-28 13:34:17 +0530

    Add database integration and sample data script

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const app = express()
 const articleRouter = require("./routes/articles")
 
@@ -7,28 +8,11 @@ mongoose.connect('mongodb://localhost/blog')
 
 app.set('view engine','ejs')
 app.use(express.urlencoded({ extended: false }))
-
-
-app.get("/",(req,res)=>{
-    const articles = [
-        {
-            title: "Welcome to My Blog",
-            date: new Date(),
-            description: "This is my first article about web development"
-        },
-        {
-            title: "Learning Express.js",
-            date: new Date(),
-            description: "A beginner's guide to building web applications with Express"
-        },
-        {
-            title: "Working with EJS Templates",
-            date: new Date(),
-            description: "How to create dynamic web pages using EJS templating"
-        }
-    ]
-    res.render("articles/index",{articles:articles})
-})
 app.use('/articles',articleRouter)
+
+app.get("/", async (req,res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render("articles/index", { articles: articles })
+})
 
 app.listen(3000)
