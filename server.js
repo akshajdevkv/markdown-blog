36f2af07f1d02031d2379edb204041579a418e1a
Akshaj Dev <akshajdevkv@gmail.com>
2025-05-28 12:36:27 +0530

    feat: Setup basic blog structure with Bootstrap styling - Added Bootstrap CSS/JS, article cards with title and date, New Article button, proper spacing and styling, fixed router implementation

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express =require('express')
 const app = express()
-
+const articleRouter =  require("./routes/articles")
 app.set('view engine','ejs')
-
+app.use('/articles',articleRouter)
 app.get("/",(req,res)=>{
-    res.render("index.ejs")
+    const articles = [
+        {
+            title: "Welcome to My Blog",
+            date: new Date(),
+            description: "This is my first article about web development"
+        },
+        {
+            title: "Learning Express.js",
+            date: new Date(),
+            description: "A beginner's guide to building web applications with Express"
+        },
+        {
+            title: "Working with EJS Templates",
+            date: new Date(),
+            description: "How to create dynamic web pages using EJS templating"
+        }
+    ]
+    res.render("index.ejs",{articles:articles})
 })
 
 app.listen(3000)
